3f5774d600c2bbdbc821594d5ed7aff725d4ed17
Somecode <62828358+Somecode14@users.noreply.github.com>
2025-07-01 18:09:52 +0700

    Added echo

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 import telebot
 
@@ -6,6 +7,18 @@ bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Hello")
+    bot.reply_to(message, "Добро пожаловать! Отправьте файл .xlsx для добавления информации в базу.\nИли введите /start, чтобы показать это сообщение ещё раз.")
+    log_interaction(message, "Sent /start")
+
+@bot.message_handler()
+def msg(message):
+    bot.send_message(message.chat.id, message.text)
+    log_message(message)
+
+def log_interaction(message, text):
+    logging.info(f"<{message.from_user.id}>: {text}")
+
+def log_message(message):
+    logging.info(f"<{message.from_user.id}>: {message.text}")
 
 bot.infinity_polling()
