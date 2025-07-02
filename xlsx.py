51027b9fcd3505aedff723a727fa073e9a069a58
Somecode <62828358+Somecode14@users.noreply.github.com>
2025-07-02 14:51:49 +0700

    The bot now shows the amount of rows affected

--- xlsx.py
+++ xlsx.py
@@ -12,11 +12,13 @@ def get_worksheet(file, message, sz_number):
     global database
     workbook = pandas.read_excel(file)
     new_rows = pandas.DataFrame()
+    new_cell_names = []
     for row in workbook.index:
         if workbook.loc[row]["CellName"] in cell_names:
             logging.warning(f"CellName {workbook.loc[row]["CellName"]} is already in the database.")
         else:
             logging.info(f"Adding CellName {workbook.loc[row]["CellName"]} to the database")
+            new_cell_names.append(workbook.loc[row]["CellName"])
 
             # BsNumber
 
@@ -43,6 +45,9 @@ def get_worksheet(file, message, sz_number):
             new_row = pandas.Series(data = {"CellName": workbook.loc[row]["CellName"], "BsNumber": bs_number, "Стандарт": "?", "BSID": bsid, "Филиал": "?", "CustomStatus": "?", "СЗ_Number": sz_number, "StartTime": "?", "EndTime": "?", "Время изменения": "?", "Автор": "?"})
             new_rows = pandas.concat([new_rows, new_row.to_frame().T])
             cell_names.add(workbook.loc[row]["CellName"])
-    database = pandas.concat([database, new_rows])
-    database.to_excel("database/database_new.xlsx", index=False)
-    bot.bot.reply_to(message, "Изменения внесены в базу.")
+    if new_cell_names:
+        database = pandas.concat([database, new_rows])
+        database.to_excel("database/database_new.xlsx", index=False)
+        bot.bot.reply_to(message, f"Добавлено {len(new_cell_names)} записей в базу: {new_cell_names}.")
+    else:
+        bot.bot.reply_to(message, "Служебная записка не содержит новых записей. Нет изменений в базе.")
